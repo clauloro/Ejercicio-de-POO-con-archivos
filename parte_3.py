f4ad8b1083664dfcee9f3fbab49c5819f807773a
Enriquepardo <epardser@myuax.com>
2023-04-16 17:57:58 +0200

    .

--- parte_3.py
+++ parte_3.py
@@ -1,12 +1,25 @@
-from parte_1 import *
-from parte_2 import *
+from parte_1 import calcular_notas, calificaciones
+
 
 def separar_alumnos(lista_alumnos_con_notas):
     aprobados = []
     suspensos = []
     for alumno in lista_alumnos_con_notas:
-        if float(alumno['Asistencia'].replace(',', '.')) >= 0.75 and float(alumno['Parcial1'].replace(',', '.')) >= 4 and float(alumno['Parcial2'].replace(',', '.')) >= 4 and float(alumno['NotaFinal'].replace(',', '.')) >= 5:
+        if float(alumno['Asistencia'].replace(',', '.').replace('%', '')) / 100 >= 0.75 and float(alumno['Parcial1'].replace(',', '.')) >= 4 and float(alumno['Parcial2'].replace(',', '.')) >= 4 and float(alumno['NotaFinal']) >= 5:
             aprobados.append(alumno)
         else:
             suspensos.append(alumno)
     return aprobados, suspensos
+
+
+def main():
+    calificaciones_alumnos = calificaciones()
+    lista_alumnos_con_notas = calcular_notas(calificaciones_alumnos)
+    aprobados, suspensos = separar_alumnos(lista_alumnos_con_notas)
+    print('La lista de alumnos aprobados es: \n')
+    print(aprobados)
+    print('==================================================')
+    print('La lista de alumnos suspensos es: \n')
+    print(suspensos)
+
+main()
