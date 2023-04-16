dc7a276f5b501c8df9506e018f8266c6a7b59f34
Martina Garcia <martinagg@myuax.com>
2023-04-16 13:20:48 +0200

    parte 1

--- parte1.py
+++ parte1.py
@@ -1,2 +1,14 @@
 import csv 
-with open ("c")
+def calificaciones():
+    with open ("calificaciones.csv","r") as archivo:
+        lista_alumnos=[]
+        reader=csv.DictReader(archivo,delimiter=";")
+        next(reader)
+        for row in reader:
+                lista_alumnos.append(row)
+        lista_alumnos.sort(key=lambda x: x["Apellidos"])
+        return lista_alumnos
+
+calificaciones_alumnos=calificaciones()
+for alumno in calificaciones_alumnos:
+    print(alumno)
